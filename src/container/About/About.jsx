8a634f90bcee9f87180f332712eaf529b0d71900
ann <dorisann.me@gmail.com>
2023-08-20 19:57:16 +0000

    update: about page

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -21,10 +21,11 @@ const About = () => {
           <br />
           My enthusiasm for exploration extends beyond technology. A testament
           to this is my statement to my sister that I would readily volunteer
-          for NASA's exploratory missions to new worlds, an offer that remains
-          open. This eagerness to embrace the unknown mirrors my proactive
-          approach to professional growth. Whether contributing within a team or
-          working independently, I am dedicated to achieving impactful results.
+          for NASA's exploratory missions to new worlds. (NASA if you are seeing
+          this, my offer is still very much open). This eagerness to embrace the
+          unknown mirrors my proactive approach to professional growth. Whether
+          contributing within a team or working independently, I am dedicated to
+          achieving impactful results.
           <br />
           <br />
           Away from the keyboard, I find solace in restful slumber or watching
